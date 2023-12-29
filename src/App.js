7e9d73d54597fc6ef3cb369984a99a2d912f9924
Kj Carter <kobecarter00@gmail.com>
2023-12-29 02:34:29 -0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Routes, useNavigate} from 'react-router-dom';
+
 
 function App() {
+  const navigate = useNavigate();
+
   return (
+    
+
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <header>
+        <h2>Friends Database</h2>
+        <Link className='link' to="/login">Login</Link>
+        <Link className='link' to="/friends">Friends List</Link>
+        <Link className='link' to="/friends/add">Add Friends</Link>
+        <Link className='link' to="/logout">Logout</Link>
+      </header>
+      
+      {
+
+
+
+      }
+      <Routes>
+        <Route path= '/login' element={<Login push={navigate} />}/>
+        <Route path= '/' element= {<Login push={navigate} />}/>
+        {/* { PrivateRoute( { component: <Friends />, path: '/friends'} ) } */}
+        <Route path='/friends' element={<PrivateRoute><Friends/></PrivateRoute> }/>
+        <Route path= '/logout' element= {<Logout/>}/>
+        <Route path= '/friends/add' element= {<AddFriend/>}/>
+      </Routes>
     </div>
+    
   );
 }
 
